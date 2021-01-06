b78d87cd8df13f54f4b8a2f817f5be14f37bcfbe
Anu <anubhuti.mishra2@gmail.com>
2021-01-06 12:59:06 +0530

    my first commit

---
+++ src/main/java/com/aws/awselasticbeanstakexample/AwsElasticBeanstakExampleApplication.java
@@ -0,0 +1,13 @@
+package com.aws.awselasticbeanstakexample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AwsElasticBeanstakExampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AwsElasticBeanstakExampleApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/aws/awselasticbeanstakexample/AwsElasticBeanstakExampleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.aws.awselasticbeanstakexample;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AwsElasticBeanstakExampleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
